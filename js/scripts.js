bfcbfe58943aac726bf899deb15e026609abfe36
karol <karol.pasierb@op.pl>
2017-06-10 18:26:58 +0200

    java-functions

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 8);
+var triangle3Area = getTriangleArea(5, 10);
+
+function getTriangleArea(a,h) {
+    
+	if (a <= 0) {
+    return "Nieprawidłowe dane";
+	} else if (h <= 0) {
+    return "Nieprawidłowe dane";
+	}
+
+    x = a * h / 2;
+    return x;
+}
+
+
+console.log("ogólny wynik funkcji wynosi " + getTriangleArea(10,6) ); 
+console.log("a triangle1Area wynosi " + triangle1Area);
+console.log("z kolei triangle2Area wynosi " + triangle2Area);
+console.log("natomiast triangle3Area wynosi " + triangle3Area);
